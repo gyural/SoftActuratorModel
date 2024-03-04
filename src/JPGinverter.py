be6ad25c1c317680a8a52e04c2e47afffabab464
gyural <dla93190@naver.com>
2024-03-04 15:37:29 +0900

    feat: ãjpg 불러오기 성공

--- src/JPGinverter.py
+++ src/JPGinverter.py
@@ -1,1 +1,9 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+sampleIMG = cv2.imread(f'C:\Users\Hilal\pycharmProjects\softacturatorModel\testIMG\testIMG.jpg'
+                       , cv2.IMREAD_GRAYSCALE)
+plt.imshow(sampleIMG, cmap="gray"), plt.axis("off")
+plt.show()
 print("JPG Inverting!!!")
