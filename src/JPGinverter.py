de0da8b840ebcbd1408b38158b955f4e14f00313
gyural <dla93190@naver.com>
2024-03-04 16:10:27 +0900

    feat: 디렉토리 이미지ë들 불러오기

--- src/JPGinverter.py
+++ src/JPGinverter.py
@@ -1,9 +1,14 @@
 import cv2
+import os
 import numpy as np
 from matplotlib import pyplot as plt
 
-sampleIMG = cv2.imread(f'C:\Users\Hilal\pycharmProjects\softacturatorModel\testIMG\testIMG.jpg'
-                       , cv2.IMREAD_GRAYSCALE)
-plt.imshow(sampleIMG, cmap="gray"), plt.axis("off")
-plt.show()
-print("JPG Inverting!!!")
+IMGdir = "C:\\Users\\Hilal\\pycharmProjects\\softacturatorModel\\testIMG"
+os.chdir(IMGdir) # 해당 폴더로 이동
+IMGfiles = os.listdir(IMGdir) # 해당 폴더에 있는 파일 이름을 리스트 형태로 받음
+
+dataList = []
+for img in IMGfiles:
+    dataList.append(IMGdir + "\\" + cv2.imread(img))
+
+plt.show(dataList[0])
