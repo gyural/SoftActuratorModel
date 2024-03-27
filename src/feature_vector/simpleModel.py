64a57f52b24f46e57a9323d62d86e0d0cffe0a70
gyural <dla9319@korea.ac.kr>
2024-03-27 12:16:39 +0900

    train: 800epoch simplemodel

--- src/feature_vector/simpleModel.py
+++ src/feature_vector/simpleModel.py
@@ -1,49 +1,71 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F                       # torch 내의 세부적인 기능을 불러옴.
 import torch.optim as optim
 from parsingPoints import get_feature_points
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import TensorDataset, DataLoader
+import simpleModel_load
 
-# 임의의 데이터 생성
-feature_points = get_feature_points()
-X = torch.tensor(feature_points)  # 입력 데이터, 크기: [100, 33, 2]
-Y = 3 * X.mean(dim=(1, 2)).unsqueeze(1) # 타겟 데이터, 크기: [100, 1]
-# 모델 정의
 class SimpleModel(nn.Module):
     def __init__(self):
-        super(SimpleModel, self).__init__()
-        self.linear = nn.Linear(66, 1)  # 입력 차원: 66, 출력 차원: 1
-        self.sigmoid = nn.ReLU()
+        super().__init__()  # 모델 연산 정의
+        self.fc1 = nn.Linear(66, 50, bias=True)  # 입력층(66) -> 은닉층1(50)으로 가는 연산
+        self.fc2 = nn.Linear(50, 30, bias=True)  # 은닉층1(50) -> 은닉층2(30)으로 가는 연산
+        self.fc6 = nn.Linear(30, 1, bias=True)  # 출력층(30) -> 출력층(1)으로 가는 연산
+        self.dropout = nn.Dropout(0.2)  # 연산이 될 때마다 20%의 비율로 랜덤하게 노드를 없앤다.
+
     def forward(self, x):
-        x = x.view(66)  # 입력 데이터의 형태를 [batch_size, 66]으로 변경합니다.
-        x = x.sigmoid()
-        return self.linear(x)
+        x = x.view(-1, 66)
 
-# 모델 초기화
-model = SimpleModel()
-
-# 손실 함수와 옵티마이저 정의
-criterion = nn.MSELoss()
-optimizer = optim.SGD(model.parameters(), lr=0.01)
-
-# 데이터셋 정의
-dataset = TensorDataset(X, Y)
+        x = F.relu(self.fc1(x))  # Linear 계산 후 활성화 함수 ReLU를 적용한다.
+        x = self.dropout(F.relu(self.fc2(x)))  # 은닉층2에서 드랍아웃을 적용한다.(즉, 30개의 20%인 6개의 노드가 계산에서 제외된다.)
+        x = F.relu(self.fc6(x))  # Linear 계산 후 활성화 함수 ReLU를 적용한다.
+        return x
 
 if __name__ == "__main__":
+    # 임의의 데이터 생성
+    feature_points = get_feature_points()
+    X = torch.tensor(feature_points)  # 입력 데이터, 크기: [100, 33, 2]
+    # Y = 3 * X.mean(dim=(1, 2)).unsqueeze(1)  # 입력 데이터와 동일한 형태로 타겟 데이터 생성
+    Y = torch.tensor(simpleModel_load.get_IMG_bandingAngle()).unsqueeze(1)
+    # 모델 초기화
+    model = SimpleModel()
+    # 손실 함수와 옵티마이저 정의
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.0001, weight_decay=1e-7)
+    # 데이터셋 및 데이터로더 정의
+    dataset = TensorDataset(X, Y)
+    dataloader = DataLoader(dataset, batch_size=2, shuffle=True)
+
+
     # 훈련
-    num_epochs = 10
-    for epoch in range(num_epochs):
-        # Forward pass
-        for i in range(len(dataset)):  # 데이터셋의 길이만큼 순회
-            inputs, targets = dataset[i]
-            # print(inputs.size(), targets.size())
-            outputs = model(inputs)  # 입력 데이터를 그대로 사용
-            if (i == len(dataset) - 1):
-                print(outputs, targets)
+    loss_ = []
+    num_epochs = 8000
+    for epoch in range(1, num_epochs+1):
+        total_loss = 0.0  # 전체 손실 초기화
+        for inputs, targets in dataloader:  # 데이터로더를 통해 배치 단위로 데이터를 가져옴
+            optimizer.zero_grad()  # optimizer의 gradient를 초기화하여 중복 계산을 방지
+
+            outputs = model(inputs)
             loss = criterion(outputs, targets)
-            # Backward pass 및 경사하강법
-            optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-        # 로그 출력
-        print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}')
+
+            total_loss += loss.item()  # 각 배치의 손실을 누적
+            # Backward pass 및 경사하강법
+
+        # 평균 손실 계산 및 출력
+        loss_.append(total_loss/len(dataloader))
+        if(epoch % 100 == 0):
+            print(f'Epoch [{epoch}/{num_epochs}], Average Loss: {total_loss/len(dataloader):.4f}')
+
+    # 모델 저장하기
+    torch.save(model.state_dict(), 'feature_vector_model.pth')
+    print("##############학습 완료############")
+    #### 저장 이후 출력 테스팅
+    for inputs, targets in dataloader:
+        outputs = model(inputs)
+        for a, b in zip(outputs, targets):
+            print(a.item(), b.item())  # 출력값 및 타겟값을 스칼라로 변환하여 출력
+        loss = criterion(outputs, targets)
+        print(f'Loss: {loss.item():.4f}')
